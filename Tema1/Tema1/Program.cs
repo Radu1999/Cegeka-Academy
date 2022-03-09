2a8258fef65ec778d7f04cb033e1ac3be6ce77ee
Radu1999 <37249331+Radu1999@users.noreply.github.com>
2022-03-09 08:52:15 +0200

    Added project

---
+++ Tema1/Tema1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Set<int> set = new Set<int>();
+set.Insert(12);
+set.Insert(13);
+set.Insert(14);
+
+try
+{
+    set.Insert(12);
+} catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine(set);
+
+Set<int> set2 = set.Filter(x => x % 2 == 0);
+
+Console.WriteLine(set2);
+
+set2.Insert(7);
+set2.Insert(8);
+
+Set<int> set3 = set2.Merge(set);
+Console.WriteLine(set3);
+
---
+++ Tema1/Tema1/Set.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+public class Set<T>
+{
+	private HashSet<T> set;
+    public Set()
+    {
+        set = new HashSet<T>();
+    }
+	public void Insert(T item)
+    {
+        int len = set.Count;
+        set.Add(item);
+        if(len == set.Count)
+        {
+            throw new InvalidOperationException("Element already in Set");
+        }
+    }
+    public void Remove(T item)
+    {
+        set.Remove(item);
+    }
+
+    public bool Containts(T item)
+    {
+        return set.Contains(item);
+    }
+    public Set<T> Merge(Set<T> other)
+    {
+        Set<T> result = new Set<T>();
+        foreach (T item in other.set)
+        {
+            result.Insert(item);
+        }
+
+        foreach (T item in set)
+        {
+            if(!result.Containts(item))
+            {
+                result.Insert(item);
+            }
+        }
+
+        return result;
+    }
+
+    public Set<T> Filter(Func<T, bool> filter)
+    {
+        Set<T> result = new Set<T>();
+        foreach (T item in set)
+        {
+            if(filter(item))
+            {
+                result.Insert(item);
+            }
+        }
+        return result;
+    }
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (T item in set)
+        {
+            if(item != null)
+            sb.Append(item.ToString()).Append(' ');
+        }
+        return sb.ToString();
+    }
+}
