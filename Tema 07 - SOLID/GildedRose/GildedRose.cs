13c3701e7344d2fbf6c80ac3cbd099dd457a424c
Radu1999 <37249331+Radu1999@users.noreply.github.com>
2022-04-05 19:48:17 +0300

    SOLID

---
+++ Tema 07 - SOLID/GildedRose/GildedRose.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class GildedRose
+    {
+        private const string AgedBrie = "Aged Brie";
+        private const string Backstage = "Backstage passes to a TAFKAL80ETC concert";
+        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string Conjured = "Conjured, idk";
+
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            //aplicati Factory Pattern on init
+            IList<Item> mItems = new List<Item>();
+            foreach (Item item in Items)
+            {
+                var (name, sellIn, quality) = (item.Name, item.SellIn, item.Quality);
+                ItemFactory.ItemType type = ItemFactory.ItemType.Unknown;
+                switch(item.Name)
+                {
+                    case AgedBrie:
+                        type = ItemFactory.ItemType.AgedBrie;
+                        break;
+                    case Backstage:
+                        type = ItemFactory.ItemType.Backstage;
+                        break;
+                    case Sulfuras:
+                        type = ItemFactory.ItemType.Sulfuras;
+                        break;
+                    case Conjured:
+                        type = ItemFactory.ItemType.Conjured;
+                        break;
+                }
+
+                mItems.Add(ItemFactory.createItem(type, name, sellIn, quality));
+            }
+            this.Items = mItems;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                item.ChangeDay();
+                item.updateQuality();
+            }
+        }
+
+        public Item getItem(int pos)
+        {
+            if(pos >= Items.Count)
+            {
+                return null;
+            }
+            return Items[pos];
+        }
+    }
+}
---
+++ Tema 07 - SOLID/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Theory]
+        [InlineData("Aged Brie", 15, 3)]
+        public void GivenAgedBrieTypeWhenUpdateQualityThenIncrementQuality(string name, int sellIn, int quality)
+        {
+            var item = new Item(name, sellIn, quality);
+            IList<Item> Items = new List<Item>();
+            Items.Add(item);
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(4, app.getItem(0).Quality);
+        }
+
+        [Theory]
+        [InlineData("Conjured, idk", 15, 3)]
+        public void GivenConjuredTypeWhenUpdateQualityThenDecrementQualityTwiceAsFast(string name, int sellIn, int quality)
+        {
+            var item = new Item(name, sellIn, quality);
+            IList<Item> Items = new List<Item>();
+            Items.Add(item);
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(1, app.getItem(0).Quality);
+        }
+
+        [Theory]
+        [InlineData("Sulfuras, Hand of Ragnaros", 15, 3)]
+        public void GivenSulfurasTypeWhenUpdateQualityThenQualityUnchanged(string name, int sellIn, int quality)
+        {
+            var item = new Item(name, sellIn, quality);
+            IList<Item> Items = new List<Item>();
+            Items.Add(item);
+
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(3, app.getItem(0).Quality);
+        }
+    }
+}
